5d9f943839150bcb783faf6d9392b0097969ecfb
paes-mario <96839726+paes-mario@users.noreply.github.com>
2022-01-29 14:55:11 -0300

    Add files via upload

---
+++ circle.tcl
@@ -0,0 +1,10 @@
+# start of script
+
+set pi 3.1415; # assign variable pi
+set radius 2; # radius of 2 inches
+
+puts "The circunference is: [expr 2*$pi*$radius] inches."; # print circunference
+
+# end of script
+
+
